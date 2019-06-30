23404942610f61166fe3339e72bceecada80e2cb
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-30 07:57:53 +0530

    Update providers.tf

--- provisioning/providers.tf
+++ provisioning/providers.tf
@@ -10,18 +10,6 @@ provider "google-beta" {
   region  = "${var.region}"
 }
 
-provider "local" {
-  version = "~> 1.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
 provider "kubernetes" {
   version          = "~> 1.7"
   load_config_file = true
