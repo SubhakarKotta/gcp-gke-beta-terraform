9257dbf714664643a25937a56fd78e3681c380b0
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-30 12:15:24 +0530

    Init

---
+++ provisioning/HELM_tiller.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+
+  depends_on = ["module.cluster", "module.db"]
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  depends_on = [
+    "kubernetes_service_account.tiller",
+  ]
+}
+
+resource "helm_release" "mydatabase" {
+  depends_on = [
+    "kubernetes_cluster_role_binding.tiller",
+  ]
+
+  name  = "mydatabase"
+  chart = "stable/mariadb"
+
+  set {
+    name  = "mariadbUser"
+    value = "foo"
+  }
+
+  set {
+    name  = "mardiadbPassword"
+    value = "qux"
+  }
+}
--- provisioning/providers.tf
+++ provisioning/providers.tf
@@ -16,3 +16,17 @@ provider "kubernetes" {
   token                  = "${data.google_client_config.default.access_token}"
   cluster_ca_certificate = "${base64decode(module.cluster.ca_certificate)}"
 }
+
+provider "helm" {
+  version         = "~> 0.10"
+  install_tiller  = true
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.13.1"
+  service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+  namespace       = "${kubernetes_service_account.tiller.metadata.0.namespace}"
+
+  kubernetes {
+    host                   = "https://${module.cluster.endpoint}"
+    token                  = "${data.google_client_config.default.access_token}"
+    cluster_ca_certificate = "${base64decode(module.cluster.ca_certificate)}"
+  }
+}
